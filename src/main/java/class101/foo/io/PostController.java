a6ab5f9c2e5ccc9e61b1b20ad67696eac7114816
ksh9241 <ksh9241@naver.com>
2021-05-15 18:54:31 +0900

    feat: paging 적용

--- src/main/java/class101/foo/io/PostController.java
+++ src/main/java/class101/foo/io/PostController.java
@@ -1,12 +1,16 @@
 package class101.foo.io;
 
 import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.data.domain.Page;
+import org.springframework.data.domain.PageRequest;
+import org.springframework.data.domain.Sort;
 import org.springframework.web.bind.annotation.*;
 
 import java.util.List;
 
 @RestController
 public class PostController {
+    private static int PAGE_SIZE = 20;
 
     @Autowired
     PostRepository postRepository;
@@ -17,13 +21,17 @@ public class PostController {
         return postRepository.save(post);
     }
 
-    // 2-1. 글 목록을 조회한다.
+    // 2. 글 목록을 페이징하여 반환
     @GetMapping("/posts")
-    public List<Post> getPostList() {
-        return postRepository.findAll();
+    public Page<Post> getPostList(@RequestParam(defaultValue="1")int page) {
+        return postRepository.findAll(
+                PageRequest.of(page -1,PAGE_SIZE, Sort.by("id").descending())
+                //page -1 을 하는 이유는 페이지가 index라서 0번부터 시작하게 되는데 defaultValue가 1이기 때문에 -1을 해주는 것이다. 또한 넘어오는 page의
+                //데이터 역시 1부터 시작하기 때문이다.
+        );
     }
-    
-    // 2-2 글 목록을 페이징하여 반환
+
+
     
     // 3. 글 번호로 조회
     
