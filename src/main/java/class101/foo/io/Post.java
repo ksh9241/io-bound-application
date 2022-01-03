f4d31b70070adb9202cafb828100e6b006b456ec
ksh9241 <ksh9241@naver.com>
2022-01-03 22:13:35 +0900

    feat:db에서 es를 사용하도록 변경

--- src/main/java/class101/foo/io/Post.java
+++ src/main/java/class101/foo/io/Post.java
@@ -8,8 +8,6 @@ import javax.persistence.*;
 @Entity(name = "post")
 public class Post {
     @Id
-    @GeneratedValue(strategy = GenerationType.SEQUENCE, generator = "post_id_seq")
-    @SequenceGenerator(name="post_id_seq", sequenceName = "post_id_seq", allocationSize = 1)
-    private Long id;
+    private String id;
     private String content;
 }
--- src/main/java/class101/foo/io/PostController.java
+++ src/main/java/class101/foo/io/PostController.java
@@ -12,7 +12,6 @@ import java.util.List;
 
 @RestController
 public class PostController {
-    private static int PAGE_SIZE = 20;
 
     @Autowired
     PostRepository postRepository;
@@ -23,8 +22,6 @@ public class PostController {
     @Autowired
     ObjectMapper objectMapper;
 
-    @Autowired
-    PostCacheService postCacheService;
 
     // 1. 글을 작성한다.
     @PostMapping("/post")
@@ -33,28 +30,6 @@ public class PostController {
         product.sendTo(jsonString);
 
         return null;
-    }
-
-    // 2. 글 목록을 페이징하여 반환
-    @GetMapping("/posts")
-    public Page<Post> getPostList(@RequestParam(defaultValue="1")int page) {
-        if ("1".equals(page)) {
-            // 크론표현식을 사용하여 1페이지를 호출할 시 항상 1페이지의 값을 가지고 있는 반환값을 리턴함.
-            return postCacheService.getFirstPostPage();
-        }
-        else {
-            return postRepository.findAll(
-                    PageRequest.of(page -1,PAGE_SIZE, Sort.by("id").descending())
-                    //page -1 을 하는 이유는 페이지가 index라서 0번부터 시작하게 되는데 defaultValue가 1이기 때문에 -1을 해주는 것이다. 또한 넘어오는 page의
-                    //데이터 역시 1부터 시작하기 때문이다.
-            );
-        }
-    }
-
-    // 3. 글 번호로 조회
-    @GetMapping("/post/{id}")
-    public Post getPostById(@PathVariable("id") Long id){
-        return postRepository.findById(id).get(); //findById만 할 경우 타입이 Post가 아니기 때문에 .get()으로 Post타입으로 변환한다.
     }
     
     // 4. 글 내용으로 검색 -> 해당 내용이 포함된 모든 글
